d7388e92daf458d5438caff96d6c330cd9c131a2
Fiolcyk <Fiolcyk@gmail.com>
2019-03-29 16:46:35 +0100

    first

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { KontaktComponent } from './components/kontakt/kontakt.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
-const routes: Routes = [];
+  
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'quiz', component: QuizComponent},
+  { path: 'kontakt', component: KontaktComponent},
+  { path: 'blog', component: BlogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { KontaktComponent } from './components/kontakt/kontakt.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    QuizComponent,
+    KontaktComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
