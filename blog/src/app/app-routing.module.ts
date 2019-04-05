46723cce7cf94f78e28f2891a55c8deb4a69ce7b
Fiolcyk <Fiolcyk@gmail.com>
2019-04-05 16:49:41 +0200

    pipes,services

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { KontaktComponent } from './components/kontakt/kontakt.component';
 import { QuizComponent } from './components/quiz/quiz.component';
-
-  
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
+import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'quiz', component: QuizComponent},
   { path: 'kontakt', component: KontaktComponent},
-  { path: 'blog', component: BlogComponent},
+  { path: 'blog', component: BlogHomeComponent},
+  { path: 'blog/detail/:id', component: BlogItemDetailsComponent}
 ];
 
 @NgModule({
